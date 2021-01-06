a7b58cf87b49da3411c6d1d813baa7c47fb4c8cf
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 16:13:40 -0500

    Add imports and provider for question

--- src/components/Sourvey/Sourvey.tsx
+++ src/components/Sourvey/Sourvey.tsx
@@ -1,1 +1,17 @@
-export default {}
+import React, {useContext} from 'react';
+import {AnswerContext, QuestionContext} from '../../types';
+import {Question} from '../Question/Question';
+
+
+export const Sourvey: React.FC = () => {
+  let thisContext = useContext(QuestionContext)
+  console.log(thisContext)
+  return (
+    <AnswerContext.Provider value={[]}>
+      <div>
+        <Question />
+      </div>
+     </AnswerContext.Provider>
+  );
+};
+
