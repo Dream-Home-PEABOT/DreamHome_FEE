c0038855fc6c282363e5e8a4997bca39587efa75
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 12:26:18 -0500

    Add home to test useContext state management

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,2 +1,13 @@
-export default {}
+import React, {useContext} from 'react';
+import {Context} from '../../types';
+
+export const Home: React.FC = () => {
+  let thisContext = useContext(Context)
+  console.log(thisContext)
+  return (
+    <div>
+      {"hi"}
+    </div>
+  );
+};
 
