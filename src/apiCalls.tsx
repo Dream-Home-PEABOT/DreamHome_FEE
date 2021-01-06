01abdd2af1572068d38cf2ed4a9d407dcd0a8240
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 12:25:20 -0500

    Add dummy api call to test context

---
+++ src/apiCalls.tsx
@@ -0,0 +1,32 @@
+//export const cleanResponse = (character: FetchedCharacter): CleanedCharacter => {
+//  return {
+//    images: {
+//      characterIcon: character.images.icon,
+//      portrait: character.images.portrait,
+//      seriesIcon: character.series.icon,
+//    },
+//    name: character.name,
+//    seriesName: character.series.name,
+//    tier: 0,
+//    wins: 0,
+//    losses: 0,
+//    isMained: false
+//  }
+//}
+//interface QuestionFormat {
+//  images: {
+//    characterIcon: string;
+//    portrait: string;
+//    seriesIcon: string;
+//  };
+//  name: string;
+//  seriesName: string;
+//  tier: number;
+//  wins: number;
+//  losses: number;
+//  isMained: boolean;
+//}
+export const getUserSkins = () => {
+  const url:string = "https://my-json-server.typicode.com/typicode/demo/db"
+  return fetch(url).then(data => data.json())
+}
