074013a57acc7a170ff786430f3d2f7b121d994c
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 17:26:55 -0500

    Adjust interfact to be more descriptive of data object

--- src/types.tsx
+++ src/types.tsx
@@ -4,15 +4,28 @@ export interface QuestionNode{
   comments: any[],
   posts: any[]
 }
-
-
 export interface QuestionFormat{
+  data:{
   type: string;
   question: string;
   description: string;
   information: string;
   note: string;
   source: string;
+  }
 }
-export const QuestionContext = React.createContext<Partial<QuestionFormat | null>>({})
+export interface AllQuestionFormat{
+  annual_salary:QuestionFormat;
+  zipcode:QuestionFormat;
+  monthly_debt:QuestionFormat;
+  downpayment_savings:QuestionFormat;
+  credit_score_range:QuestionFormat;
+  downpayment_percentage:QuestionFormat;
+  mortgage_term:QuestionFormat;
+  goal_home_price:QuestionFormat;
+  external_services:QuestionFormat;
+  survey:QuestionFormat;
+}
+
+export const QuestionContext = React.createContext<Partial<AllQuestionFormat | null>>({})
 export const AnswerContext = React.createContext<Partial<[]>>([])
