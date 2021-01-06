b39444ba7bdda9f1ad1cc14e91a7e4ab9c5221bf
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 18:55:58 -0500

    Add value for provider to pass down

--- src/components/Sourvey/Sourvey.tsx
+++ src/components/Sourvey/Sourvey.tsx
@@ -1,15 +1,25 @@
-import React, {useContext, useState} from 'react';
+import React, {useEffect, useContext, useState} from 'react';
 import {AnswerContext, QuestionContext} from '../../types';
 import {Question} from '../Question/Question';
 
 
 export const Sourvey: React.FC = () => {
-  let thisContext = useContext(QuestionContext)
-  let theseKeys = thisContext ? Object.keys(thisContext): null;
-  console.log(theseKeys)
-  const [answerKey, updateAnswers] = useState();
+  const thisContext = useContext(QuestionContext)
+  const [answerKey, updateAnswers] = useState<any>();
+  let theseKeys: any;
+  if (thisContext) {
+    theseKeys = Object.keys(thisContext).reduce((acc: any,cur)=>{
+
+      acc[cur] = ''
+      return acc
+    },{})
+  }
+  useEffect(() => {
+    updateAnswers(theseKeys)
+  }, []);
+  
   return (
-    <AnswerContext.Provider value={[]}>
+    <AnswerContext.Provider value={answerKey}>
       <div>
         <Question />
       </div>
