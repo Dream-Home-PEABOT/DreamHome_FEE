04f639cc24533ea76b6f49349470442e498ce36d
Orlando Murcio <ato.murcio@gmail.com>
2021-01-06 16:06:39 -0700

    Display nav and jurney element

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,36 +3,42 @@ import React, { useState, useEffect } from 'react';
 //types
 import { data } from '../../types';
 //components
-import Home from '../Home/Home';
-import Error from '../Error/Error';
-import Journey from '../Journey/Journey';
+// import Home from '../Home/Home';
+// import Error from '../Error/Error';
+// import Journey from '../Journey/Journey';
 import Sourvey from '../Sourvey/Sourvey';
-import GenerateReport from '../GenerateReport/GenerateReport';
+import NavBar from '../NavBar/NavBar';
+// import GenerateReport from '../GenerateReport/GenerateReport';
 
 import './App.css';
 
-const Context = React.createContext()
+const Context = React.createContext({})
 
 
-const App: React.FC = () => {
+const App = () => {
 
-  const [data, setData] = useState<data>({});
+  const [data, setData] = useState<data>();
 
   const getQuestionsData = () => {
 
-    setData();
+    // setData();
   }
 
   return (
-    //create interface for data
-    <Context.Provider data={data}>
-      <Home />
-      <Error />
-      <Journey />
-      <Sourvey />
-      <GenerateReport />
-    </Context.Provider>
-  );
+    <>
+    {/* //create interface for data
+    // <Context.Provider data={data}>
+    //   <Home />
+    //   <Error />
+    //   <Journey /> */}
+    
+    <NavBar />
+    <Sourvey />
+    {/* //  <GenerateReport />
+    // </Context.Provider> 
+    */}
+    </>
+    );
 }
 
 export default App;
