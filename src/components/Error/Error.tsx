2048a24a5a58c0ebe00b8f081b04c246a4ffc6d4
Tim Keresey <tim.keresey@gmail.com>
2021-01-06 11:30:43 -0700

    write jsx skeleton

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -4,7 +4,12 @@ import React, { useState, useEffect } from 'react';
 const Error: React.FC = () => {
 
   return (
-
+    <div>
+      <h1>404error</h1>
+      <div>
+        Menu
+      </div>
+    </div>
   )
 }
 
