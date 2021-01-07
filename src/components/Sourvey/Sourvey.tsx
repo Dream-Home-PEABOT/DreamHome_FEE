e2e3ad63ee25330db5456f98aa379aa0ee7b97b7
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 19:06:24 -0500

    Adjust context value to only fire in useEffect

--- src/components/Sourvey/Sourvey.tsx
+++ src/components/Sourvey/Sourvey.tsx
@@ -6,18 +6,16 @@ import {Question} from '../Question/Question';
 export const Sourvey: React.FC = () => {
   const thisContext = useContext(QuestionContext)
   const [answerKey, updateAnswers] = useState<any>();
-  let theseKeys: any;
-  if (thisContext) {
-    theseKeys = Object.keys(thisContext).reduce((acc: any,cur)=>{
-
-      acc[cur] = ''
-      return acc
-    },{})
-  }
+  let theseKeys: [];
   useEffect(() => {
+    if (thisContext) {
+      theseKeys = Object.keys(thisContext).reduce((acc: any,cur)=>{
+        acc[cur] = ''
+        return acc
+      },{})
+    }
     updateAnswers(theseKeys)
   }, []);
-  
   return (
     <AnswerContext.Provider value={answerKey}>
       <div>
