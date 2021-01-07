fc25f5f19bb88aef6e1a8ed30face2c7e6dec303
Tim Keresey <tim.keresey@gmail.com>
2021-01-06 19:18:52 -0700

    add tests for Question component

--- src/components/Question/Question.test.tsx
+++ src/components/Question/Question.test.tsx
@@ -7,5 +7,40 @@ import { BrowserRouter } from 'react-router-dom';
 import Question from './Question';
 
 describe('Question', () => {
+  it('should render displaying a message', () => {
+
+  });
+
+  it('should render with a back button', () => {
+
+  });
+
+  it('should render with a next button', () => {
+
+  });
+
+  it('should render with a question', () => {
+
+  });
+
+  it('should render with an entry form', () => {
+
+  });
+
+  it('should render with a secondary message', () => {
+
+  });
+
+  it('for a user, should be able to click the back button', () => {
+
+  });
+
+  it('for a user, should be able to click the next button', () => {
+
+  });
+
+  it('for a user, the input form value should change as they fill it in', () => {
+
+  });
 
 })
