b7e74acbdfe552f9f5c0fbe99a0b3bfca8c057ca
Tim Keresey <tim.keresey@gmail.com>
2021-01-06 18:59:19 -0700

    add tests for Journey component

--- src/components/Journey/Journey.test.tsx
+++ src/components/Journey/Journey.test.tsx
@@ -7,5 +7,20 @@ import { BrowserRouter } from 'react-router-dom';
 import Journey from './Journey';
 
 describe('Journey', () => {
+  it('should render displaying a message', () => {
+
+  });
+
+  it('should render displaying a title', () => {
+
+  });
+
+  it('should render with a start button', () => {
+
+  });
+
+  it('for a user, should be able to click the button', () => {
+
+  });
 
 })
