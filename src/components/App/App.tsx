e1e66d47e568c110bb3f90a0eeaee67f9313c5f7
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-07 17:35:04 -0500

    Move survey logic for answer key to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,20 +12,28 @@ import GenerateReport from '../GenerateReport/GenerateReport';
 //create interface for context
 
 const App:React.FC = () =>{
-  const [questions, updateQuestions] = useState<AllQuestionFormat | null>(null);
+  const [questions, updateQuestions] = useState<AllQuestionFormat | {}>({});
+  let answerKey: any;
+  const buildAnswers = (questions: AllQuestionFormat | {}): void => {
+    answerKey = Object.keys(questions).reduce((acc: any,cur)=>{
+        acc[cur] = ''
+        return acc
+      },{})
+  }
   useEffect(() => {
-    getQuestions().then((data) => updateQuestions(data))
+    getQuestions().then((data) => updateQuestions(data)).then(() => buildAnswers(questions))
   }, []);
 
   return (
     <QuestionContext.Provider value={questions}>
-     
+      {/*<AnswerContext.Provider value={answerKey}>*/}
         <NavBar />
         <Home />
         <Journey />
         <Survey />
         <Question/>
         <GenerateReport/>
+      {/*</AnswerContext.Provider>*/}
      </QuestionContext.Provider>
   );
 }
