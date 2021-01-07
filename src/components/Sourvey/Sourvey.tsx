c48f9b821ac1a99b7410541cabde04547d7287f9
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 19:12:46 -0500

    Adjust questionKeys for better naming convention

--- src/components/Sourvey/Sourvey.tsx
+++ src/components/Sourvey/Sourvey.tsx
@@ -6,15 +6,15 @@ import {Question} from '../Question/Question';
 export const Sourvey: React.FC = () => {
   const thisContext = useContext(QuestionContext)
   const [answerKey, updateAnswers] = useState<AllQuestionFormat | null>(null);
-  let theseKeys: null;
+  let questionKeys: null;
   useEffect(() => {
     if (thisContext) {
-      theseKeys = Object.keys(thisContext).reduce((acc: any,cur)=>{
+      questionKeys = Object.keys(thisContext).reduce((acc: any,cur)=>{
         acc[cur] = ''
         return acc
       },{})
     }
-    updateAnswers(theseKeys)
+    updateAnswers(questionKeys)
   }, []);
   return (
     <AnswerContext.Provider value={answerKey}>
