a770eafa610d2e6762f86fcd78fc6f53912e5e88
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-07 14:15:06 -0500

    Adjust user input to correctly add to context when clicking submit button

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,19 +1,21 @@
 import React, { useState, useContext}  from 'react';
-import {AnswerContext} from '../../types';
+import {QuestionContext, AnswerContext} from '../../types';
 
   interface Props{
-    updateAllAnswers: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    updateAllAnswers: any;
     currentQuestion: string;
   }
 
   export const Question: React.FC<Props> = ({updateAllAnswers, currentQuestion}) => {
     const answerContext = useContext(AnswerContext)
+    const questionContext = useContext(QuestionContext)
+    console.log(questionContext)
     const [answerInput, updateAnswer] = useState<any>({question: '', answer: ''});
   return (
     <div>
       Question: {currentQuestion}
       <input onChange={(e)=>updateAnswer({...answerInput, answer: e.target.value})}/>
-      <button onClick={()=>updateAllAnswers({...answerInput})}></button>
+      <button onClick={()=>updateAllAnswers({...answerContext, [currentQuestion]: answerInput.answer})}></button>
     </div>
   );
 };
