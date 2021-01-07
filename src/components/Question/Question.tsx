33862dfd1966b8a1cccf4c3ffbdd32a6e6843bb3
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-07 14:04:22 -0500

    Add interface for currentQuestions

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,15 +1,19 @@
-import React, { useState}  from 'react';
+import React, { useState, useContext}  from 'react';
+import {AnswerContext} from '../../types';
 
   interface Props{
     updateAllAnswers: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    currentQuestion: string;
   }
 
-  export const Question: React.FC<Props> = ({updateAllAnswers}) => {
+  export const Question: React.FC<Props> = ({updateAllAnswers, currentQuestion}) => {
+    const answerContext = useContext(AnswerContext)
     const [answerInput, updateAnswer] = useState<any>({question: '', answer: ''});
   return (
     <div>
+      Question: {currentQuestion}
       <input onChange={(e)=>updateAnswer({...answerInput, answer: e.target.value})}/>
-      <button onClick={()=>updateAllAnswers(answerInput)}></button>
+      <button onClick={()=>updateAllAnswers({...answerInput})}></button>
     </div>
   );
 };
