35a68319ed82a3b778b5715b951de2fd7f1e82a6
Tim Keresey <tim.keresey@gmail.com>
2021-01-06 17:37:05 -0700

    add NavBar test file

---
+++ src/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+
+import NavBar from './NavBar';
+
+describe('NavBar', () => {
+
+})
