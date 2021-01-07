51aa4f3631389d6a8924d0aaec394540cc6d30e8
Tim Keresey <tim.keresey@gmail.com>
2021-01-07 10:12:35 -0700

    add tests for Report component

--- src/components/Report/Report.test.tsx
+++ src/components/Report/Report.test.tsx
@@ -7,5 +7,20 @@ import { BrowserRouter } from 'react-router-dom';
 import Report from './Report';
 
 describe('Report', () => {
+   it('should display a report of the user results', () => {
+
+   });
+
+   it('should have buttons for selecting a plan', () => {
+
+   });
+
+   it('a user should be able to click those buttons', () => {
+
+   });
+
+   it('should change the values in the report when a different button is selected', () => {
+
+   });
 
 })
