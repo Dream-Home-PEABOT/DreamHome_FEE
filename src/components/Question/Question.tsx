07cf205cb5c8d3b3afd5d89c700087240208a6b3
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 22:58:26 -0500

    Add onClick handler to update context value

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,8 +1,16 @@
-import React  from 'react';
-export const Question: React.FC = () => {
+import React, {useRef, useState}  from 'react';
+
+  interface Props{
+    handleClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  }
+
+  export const Question: React.FC<Props> = ({handleClick}) => {
+  const [answerInput, updateAnswer] = useState<any>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <div>
-      {"hi"}
+      <input ref={inputRef} onChange={(e)=>updateAnswer(e.target.value)}/>
+      <button onClick={(e)=>handleClick(answerInput)}></button>
     </div>
   );
 };
