ff67e9194302b828ffd0da4a6b4544b32770b005
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-06 19:11:08 -0500

    Adjust theseKeys starting value to null

--- src/components/Sourvey/Sourvey.tsx
+++ src/components/Sourvey/Sourvey.tsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useContext, useState} from 'react';
-import {AnswerContext, QuestionContext} from '../../types';
+import {AllQuestionFormat, AnswerContext, QuestionContext} from '../../types';
 import {Question} from '../Question/Question';
 
 
 export const Sourvey: React.FC = () => {
   const thisContext = useContext(QuestionContext)
-  const [answerKey, updateAnswers] = useState<any>();
-  let theseKeys: [];
+  const [answerKey, updateAnswers] = useState<AllQuestionFormat | null>(null);
+  let theseKeys: null;
   useEffect(() => {
     if (thisContext) {
       theseKeys = Object.keys(thisContext).reduce((acc: any,cur)=>{
