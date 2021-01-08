904a51914ea41275c6c7c022ff85e40af3269c3f
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-07 20:34:49 -0500

    Add li for link change

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,12 +3,17 @@ import './NavBar.css'
 import '../../_variables.css';
 import { ImMenu} from "react-icons/im";
 
+
 const NavBar = () => {
 
   return (
     <div className='nav-container'>
       <div className="inner-menu">
         <ImMenu className='icon'/>
+        <ul>
+          <li><a href="#">Something1</a></li>        
+        </ul>
+
         <h1 className='menu-title'>menu</h1>
       </div>
     </div>
