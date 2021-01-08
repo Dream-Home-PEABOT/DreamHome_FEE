1a682046ae84fa1a3ca3926dc31bcae92e86fb59
Tim Keresey <tim.keresey@gmail.com>
2021-01-07 17:21:31 -0700

    test rendering of Journey with all elements

--- src/components/Journey/Journey.test.tsx
+++ src/components/Journey/Journey.test.tsx
@@ -7,20 +7,27 @@ import { BrowserRouter } from 'react-router-dom';
 import Journey from './Journey';
 
 describe('Journey', () => {
-  it('should render displaying a message', () => {
+  it('should render displaying an info message', () => {
+    render(<Journey />);
 
+    expect(screen.getByText('Hi, my name is Teki and I will be here to accompany you throughout this journey. One thing you should know is that I am a vocabulary specialist.')).toBeInTheDocument();
   });
 
   it('should render displaying a title', () => {
+    render(<Journey />);
 
+    expect(screen.getByText('My')).toBeInTheDocument();
+    expect(screen.getByText('Dream Home')).toBeInTheDocument();
   });
 
   it('should render with a start button', () => {
+    render(<Journey />);
 
+    expect(screen.getByText('Start')).toBeInTheDocument();
   });
 
   it('for a user, should be able to click the button', () => {
-
+    //test onclick when implemented.
   });
 
 })
