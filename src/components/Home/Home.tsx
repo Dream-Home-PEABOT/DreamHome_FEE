a08aeeb8102d419ac6426a06074c4bb6334e87b4
Orlando Murcio <ato.murcio@gmail.com>
2021-01-10 00:59:18 -0700

    Animate title transition

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import bkg_img from '../../images/home/Big Shoes - Standing Pose.png'
-import './Home.css'
+import bkg_img from '../../images/home/Big Shoes - Standing Pose.png';
+import './Home.css';
+import {Spring} from 'react-spring/renderprops';
+
 const Home: React.FC = () => {
 
   return (
     <section className='home-section'>
       <div className="main-container">
-
-        <div className="title-container">
+        <Spring
+        from={{ opacity: 0, margin: -1000}}
+        to={{ opacity: 1, margin: 0}}
+        config={{delay: 1000, duration: 1000}}
+        >
+        {props => <div className="title-container">
           <h1 className='title'>My</h1>
           <h1 className='title-2'>Dream Home</h1>
           {/* <h2>Journey Edition</h2> */}
-        </div>
-
-        <div className="img-container">
-          <img src={bkg_img} alt="" className='main-image'/>
-        </div>
+        </div>}
+        </Spring>
+        
+        <Spring
+          from={{ opacity: 0}}
+          to={{ opacity: 1}}
+          config={{delay: 1000, duration: 1000}}>
+          {/* {props => <div>{props.number}</div>} */}
+          {props => <div style={props} className="img-container">
+            <img src={bkg_img} alt="" className='main-image'/>
+          </div>}
+        </Spring>
 
       </div>
     
