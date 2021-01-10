024748504a6247c318ae9d247cf05aad5d8f86a4
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-10 17:08:30 -0500

    Add get report api call

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -2,3 +2,8 @@ export const getQuestions = () => {
   const url:string = "http://localhost:3000/data"
   return fetch(url).then(data => data.json())
 }
+
+export const getReport = () => {
+  const url:string = "http://localhost:3000/report"
+  return fetch(url).then(data => data.json())
+}
