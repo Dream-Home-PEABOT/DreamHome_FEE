0a8f4f9a986f94364f03ed338f1e85cc16ff73a9
Tim Keresey <tim.keresey@gmail.com>
2021-01-12 13:37:57 -0700

    add backend endpoint to education api call

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,17 +1,19 @@
 export const getQuestions = async () => {
-  const response = await fetch('http://localhost:3000/data')
+  const response = await fetch('https://dreamhome-mvp.herokuapp.com/api/v1/education')
   if (response.ok) {
     return await response.json()
   } else {
     return response
   }
-}
-export const getReport = async () => {
-  const response = await fetch('http://localhost:3000/report')
-  if (response.ok) {
-    return await response.json()
-  } else {
-    return response
-  }
-}
+};
 
+//POST endpoint: 'https://dreamhome-mvp.herokuapp.com/api/v1/report'
+
+export const getReport = async (id: string) => {
+  const response = await fetch(`https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}`)
+  if (response.ok) {
+    return await response.json()
+  } else {
+    return response
+  }
+}
