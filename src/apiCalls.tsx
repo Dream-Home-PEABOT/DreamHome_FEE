d2389c26916c7f785efb4598e44f247986043b52
Tim Keresey <tim.keresey@gmail.com>
2021-01-12 13:59:07 -0700

    add back mocked api to resolve build error

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -9,11 +9,12 @@ export const getQuestions = async () => {
 
 //POST endpoint: 'https://dreamhome-mvp.herokuapp.com/api/v1/report'
 
-// export const getReport = async (id: string) => {
-//   const response = await fetch(`https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}`)
-//   if (response.ok) {
-//     return await response.json()
-//   } else {
-//     return response
-//   }
-// }
+export const getReport = async (id: string) => {
+  const response = await fetch("http://localhost:3000/report")
+  if (response.ok) {
+    return await response.json()
+  } else {
+    return response
+  }
+}
+// GET report: https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}
