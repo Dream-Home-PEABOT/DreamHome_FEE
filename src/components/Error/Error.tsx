da16a6579710f1ef63efbab030f09ffa8026077d
Orlando Murcio <ato.murcio@gmail.com>
2021-01-12 12:57:15 -0700

    Add test ids to error elements

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -13,22 +13,22 @@ const Error: React.FC<props> = ({errorNum, errorMessage}) => {
 
     <section className="error-section">
       <div className="container">
-        <div className="box-1"></div>
-        <div className="box-2"></div>
-        <div className="box-3"></div>
-        <div className="message--box">
-          <h1 className="error-message">
+        <div className="box-1" data-testid="box-1"></div>
+        <div className="box-2" data-testid="box-2"></div>
+        <div className="box-3" data-testid="box-3"></div>
+        <div className="message--box" data-testid="message box">
+          <h1 className="error-message" data-testid='error number'>
             {errorNum}Error
           </h1>
-          <h1 style={{fontSize: '2em'}} className="error-message">
+          <h1 style={{fontSize: '2em'}} className="error-message" data-testid='error message'>
             {errorMessage}
           </h1>
         </div>
         <div className="err-box-2">
-          <img src={bkg_img} alt="" className="error-image"/>
+          <img  data-testid='error image 1' src={bkg_img} alt="" className="error-image"/>
         </div>
         <div className="err-box">
-          <img src={error_img} alt="" className="error-image"/>
+          <img  data-testid='error image 2' src={error_img} alt="" className="error-image"/>
         </div>
       </div>
     </section>
