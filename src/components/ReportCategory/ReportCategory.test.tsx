a5e318db47a8fa88f0cc816d8a28beb8d076c74d
Tim Keresey <tim.keresey@gmail.com>
2021-01-12 12:50:50 -0700

    add test for rendering buttons

--- src/components/ReportCategory/ReportCategory.test.tsx
+++ src/components/ReportCategory/ReportCategory.test.tsx
@@ -39,38 +39,6 @@ describe('ReportCategory', () => {
           two: {
             monthly_savings: 200,
             goal_end_date: "12/03/2026"
-          },
-          three: {
-            monthly_savings: 300,
-            goal_end_date: "12/03/2027"
-          },
-          four: {
-            monthly_savings: 400,
-            goal_end_date: "12/03/2028"
-          },
-          five: {
-            monthly_savings: 500,
-            goal_end_date: "12/03/2029"
-          },
-          six: {
-            monthly_savings: 600,
-            goal_end_date: "12/03/2030"
-          },
-          seven: {
-            monthly_savings: 700,
-            goal_end_date: "12/03/2031"
-          },
-          eight: {
-            monthly_savings: 800,
-            goal_end_date: "12/03/2032"
-          },
-          nine: {
-            monthly_savings: 900,
-            goal_end_date: "12/03/2033"
-          },
-          ten: {
-            monthly_savings: 1000,
-            goal_end_date: "12/03/2034"
           }
         }
       }
@@ -281,6 +249,37 @@ describe('ReportCategory', () => {
       /> );
 
       expect(screen.getByText("10%")).toBeInTheDocument();
+  });
+
+  it('should display buttons for selecting a plan', () => {
+    const fakeUserReport = {
+      zip_code: 11111,
+      location: "Anywhere, CO"
+    }
+
+    const fakeReportData = Object.keys(fakeData);
+    const fakeSubtitle_2 = 'Denver';
+    const fakeKey = 0;
+
+
+    render (
+      <ReportCategory
+      position={fakeKey + 1}
+      key={fakeKey}
+      plan={fakeData.downpayment.ten_year_plan}
+      categoryName={fakeReportData[0]}
+      categoryMainNumber={fakeUserReport.zip_code || fakeData.principal.based_on_rent
+      || fakeData.monthly.monthly_principal
+      || fakeData.downpayment.down_payment_percentage_selected}
+      categoryMainTitle={'location'}
+      categorySubtitle={fakeSubtitle_2}
+      categorySecondNumber={fakeUserReport.location || fakeData.principal.goal_principal
+      || fakeData.monthly.estimated_true_monthly
+      || fakeData.downpayment.down_payment_saved}
+      categoryID={fakeKey + 1}
+      /> );
+
+      expect(screen.getByText('one yrs')).toBeInTheDocument();
   });
 
 
