13d7067948ec8e23dc96f954ccdd884dde23e89e
Tim Keresey <tim.keresey@gmail.com>
2021-01-12 13:51:56 -0700

    comment out unnedded api call for deployment test

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -9,11 +9,11 @@ export const getQuestions = async () => {
 
 //POST endpoint: 'https://dreamhome-mvp.herokuapp.com/api/v1/report'
 
-export const getReport = async (id: string) => {
-  const response = await fetch(`https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}`)
-  if (response.ok) {
-    return await response.json()
-  } else {
-    return response
-  }
-}
+// export const getReport = async (id: string) => {
+//   const response = await fetch(`https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}`)
+//   if (response.ok) {
+//     return await response.json()
+//   } else {
+//     return response
+//   }
+// }
