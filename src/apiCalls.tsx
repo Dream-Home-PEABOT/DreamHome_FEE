6e46f27817fab99e3fb22c6802adbab43354bfbf
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-13 12:17:05 -0500

    Adjust api calls for deployed endpoints

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,7 +1,7 @@
 import { Answers } from './types';
 
 export const getQuestions = async () => {
-  const response = await fetch('http://localhost:3000/data')
+  const response = await fetch('https://dreamhome-mvp.herokuapp.com/api/v1/education')
   if (response.ok) {
     return await response.json()
   } else {
@@ -12,8 +12,21 @@ export const getQuestions = async () => {
 //POST endpoint: 'https://dreamhome-mvp.herokuapp.com/api/v1/report'
 // https://dreamhome-mvp.herokuapp.com/api/v1/education
 
-export const getReport = async () => {
-  const response = await fetch("http://localhost:3000/report")
+//export const getReport = async () => {
+//  const response = await fetch("http://localhost:3000/report")
+//  if (response.ok) {
+//    return await response.json()
+//  } else {
+//    return response
+//  }
+//}
+
+export const postAnswers = async (answers: Answers) => {
+  const response = await fetch('https://dreamhome-mvp.herokuapp.com/api/v1/report', {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(answers)
+  })
   if (response.ok) {
     return await response.json()
   } else {
@@ -21,25 +34,12 @@ export const getReport = async () => {
   }
 }
 
-//export const postAnswers = async (answers: Answers) => {
-//  const response = await fetch('https://dreamhome-mvp.herokuapp.com/api/v1/report', {
-//    method: 'POST',
-//    headers: {"Content-Type": "application/json"},
-//    body: JSON.stringify(answers)
-//  })
-//  if (response.ok) {
-//    return await response.json()
-//  } else {
-//    return response
-//  }
-//}
-
-//export const getReport = async (id: string) => {
-//  const response = await fetch(`https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}`)
-//  if (response.ok) {
-//    return await response.json()
-//  } else {
-//    return response
-//  }
-//}
+export const getReport = async (id: string) => {
+  const response = await fetch(`https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}`)
+  if (response.ok) {
+    return await response.json()
+  } else {
+    return response
+  }
+}
 // GET report: https://dreamhome-mvp.herokuapp.com/api/v1/report/${id}
