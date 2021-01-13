71aadcde883456d47b37b4dd34c34321d9da4f0d
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-13 13:12:52 -0500

    Adjust calls

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,7 +1,11 @@
 import { Answers } from './types';
 
 export const getQuestions = async () => {
-  const response = await fetch('https://dreamhome-mvp.herokuapp.com/api/v1/education')
+  const response = await fetch('https://fe-cors-proxy.herokuapp.com', {
+    headers: {
+      "Target-URL": `https://dreamhome-mvp.herokuapp.com/api/v1/education`
+    }
+  })
   if (response.ok) {
     return await response.json()
   } else {
@@ -24,7 +28,9 @@ export const getQuestions = async () => {
 export const postAnswers = async (answers: Answers) => {
   const response = await fetch('https://dreamhome-mvp.herokuapp.com/api/v1/report', {
     method: 'POST',
-    headers: {"Content-Type": "application/json"},
+    headers: {
+    "Content-Type": "application/json",
+    },
     body: JSON.stringify(answers)
   })
   if (response.ok) {
