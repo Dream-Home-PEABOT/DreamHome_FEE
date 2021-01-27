aee909bc4ee1c7fc00c572edb60e38dd91fa4d5b
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-27 16:00:54 -0500

    Adjust firebase ui setup to only fire once

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,7 +2,6 @@ import firebase from "firebase/app";
 import * as firebaseui from "firebaseui";
 import "firebase/auth";
 import "./Login.css"
-import { googleProvider, googleSignInPopup } from "../../helpers/googleAuth";
 import React, { useEffect } from "react";
 
 
@@ -10,11 +9,11 @@ export const Login: React.FC<any> = () => {
   const uiSettings = {
     callbacks: {
     signInSuccessWithAuthResult: function(authResult:any, redirectUrl:any) {
-      console.log(authResult, redirectUrl)
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return false;
+      console.log(authResult.user)
+      localStorage.userUID = authResult.user.uid
+      localStorage.displayName = authResult.user.displayName
+      localStorage.email = authResult.user.email
+      return true;
     },
   },
   signInSuccessUrl: '/home',
@@ -22,14 +21,15 @@ export const Login: React.FC<any> = () => {
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID
   ],
 };
   const configureUI = () => {
-    googleProvider();
-    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+    let ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
     ui.start("#firebaseui-auth-container", uiSettings);
   };
   useEffect(() => {
+
     configureUI();
   }, []);
   return (
