d3d77aeed964c03922372e5ce2a2436ba79d48ac
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-27 17:33:22 -0500

    Dynamically render logged in / out

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,8 +7,9 @@ import { IoIosCloseCircleOutline } from "react-icons/io";
 import { FiMenu } from "react-icons/fi";
 import { NavItem } from "../NavItem/NavItem";
 import { ReportContext } from "../../helpers/context";
+import { logOut } from "../../helpers/firebase";
 
-const NavBar: React.FC = () => {
+const NavBar: React.FC<any> = (props) => {
   const [open, setOpen] = useState<boolean>(false);
 
   const closeBar = () => {
@@ -68,11 +69,12 @@ const DropdownMenu = (props: any) => {
       </Link>
 
       <Link
-        to="/login"
+        to={firebase.auth().currentUser ? "/logout" : "/login"}
         data-testid="to-login"
-        onClick={() => props.setOpen(false)}
+        onClick={() => {props.setOpen(false); logOut();}}
       >
         <DropdownItem onClick={props.setOpen}>{firebase.auth().currentUser ? "Logout": "Login"}</DropdownItem>
+
       </Link>
 
       {userReport && (
