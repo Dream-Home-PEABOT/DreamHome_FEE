112f0338ef8e4d90fcf524e4d44b119a6fa7236f
Tim Keresey <tim.keresey@gmail.com>
2021-01-27 16:52:20 -0700

    create first report section component

---
+++ src/components/ReportSecOne/ReportSecOne.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useContext } from "react";
+import { ReportContext } from "../../helpers/context";
+
+export const ReportSecOne = () => {
+
+  const reportContext = useContext(ReportContext['03_attributes']);
+
+  return (
+    <>
+
+
+    </>
+  )
+};
