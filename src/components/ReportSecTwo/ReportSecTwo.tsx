ff89f9d6d0b124378058772c1792342039727050
Tim Keresey <tim.keresey@gmail.com>
2021-01-27 18:02:13 -0700

    create component to display 2nd section of info from report

---
+++ src/components/ReportSecTwo/ReportSecTwo.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useContext } from "react";
+import { ReportContext } from "../../helpers/context";
+
+
+export const ReportSecTwo = () => {
+
+    const reportContext: any = useContext(ReportContext['03_attributes']);
+
+    return (
+      <>
+
+        <div className=''
+
+      </>
+    )
+};
