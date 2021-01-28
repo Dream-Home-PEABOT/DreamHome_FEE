58204477defef0f5345fd1c3bbfc08f11b05a712
Tim Keresey <tim.keresey@gmail.com>
2021-01-27 17:13:39 -0700

    build out circle value elements

--- src/components/ReportSecOne/ReportSecOne.tsx
+++ src/components/ReportSecOne/ReportSecOne.tsx
@@ -3,11 +3,32 @@ import { ReportContext } from "../../helpers/context";
 
 export const ReportSecOne = () => {
 
-  const reportContext = useContext(ReportContext['03_attributes']);
+  const reportContext: any = useContext(ReportContext['03_attributes']);
 
   return (
     <>
+      <section className='circles'>
+        <div className='top-circle-1'>
+          <h2>Your Credit Score</h2>
+          {reportContext.input.B_credit_score}
+        </div>
+        <div className='top-circle-1'>
+          <h2>Your Monthly Income</h2>
+          {reportContext.input.C_salary}
+        </div>
+        <div className='top-circle-1'>
+          <h2>Your Monthly Debt</h2>
+          {reportContext.input.D_monthly_debt}
+        </div>
+        <div className='top-circle-1'>
+          <h2>Your Mortgage Term</h2>
+          {reportContext.input.F_mortgage_term}
+        </div>
+      </section>
 
+      <section className='location-info'>
+        
+      </section>
 
     </>
   )
