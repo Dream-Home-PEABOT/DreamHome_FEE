5a4e56eec17a3e1823aa07a3099347f9c9f9d6e4
Tim Keresey <tim.keresey@gmail.com>
2021-01-29 13:31:41 -0700

    style email submit form

--- src/components/ReportSecOne/ReportSecOne.tsx
+++ src/components/ReportSecOne/ReportSecOne.tsx
@@ -1,44 +1,44 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ReportContext } from "../../helpers/context";
 
-export const ReportSecOne = () => {
-
-  const reportContext: any = useContext(ReportContext['03_attributes']);
-
-  return (
-    <>
-      <section className='circles'>
-        <div className='top-circle-1'>
-          <h2>Your Credit Score</h2>
-          {reportContext.input.B_credit_score}
-        </div>
-        <div className='top-circle-1'>
-          <h2>Your Monthly Income</h2>
-          ${reportContext.input.C_salary}
-        </div>
-        <div className='top-circle-1'>
-          <h2>Your Monthly Debt</h2>
-          ${reportContext.input.D_monthly_debt}
-        </div>
-        <div className='top-circle-1'>
-          <h2>Your Mortgage Term</h2>
-          {reportContext.input.F_mortgage_term} year
-        </div>
-      </section>
-
-      <section className='location-info'>
-        <div className='city-name'>
-          {reportContext.output.A_location.city_state}
-        </div>
-        <div className='zip-code'>
-          {reportContext.output.A_location.zipcode}
-        </div>
-        <div className='city-name'>
-          <h2>Average Home Price in this Area</h2>
-          ${reportContext.output.A_location.average_home_price}
-        </div>
-      </section>
-
-    </>
-  )
-};
+// export const ReportSecOne = () => {
+//
+//   const reportContext: any = useContext(ReportContext['03_attributes']);
+//
+//   return (
+//     <>
+//       <section className='circles'>
+//         <div className='top-circle-1'>
+//           <h2>Your Credit Score</h2>
+//           {reportContext.input.B_credit_score}
+//         </div>
+//         <div className='top-circle-1'>
+//           <h2>Your Monthly Income</h2>
+//           ${reportContext.input.C_salary}
+//         </div>
+//         <div className='top-circle-1'>
+//           <h2>Your Monthly Debt</h2>
+//           ${reportContext.input.D_monthly_debt}
+//         </div>
+//         <div className='top-circle-1'>
+//           <h2>Your Mortgage Term</h2>
+//           {reportContext.input.F_mortgage_term} year
+//         </div>
+//       </section>
+//
+//       <section className='location-info'>
+//         <div className='city-name'>
+//           {reportContext.output.A_location.city_state}
+//         </div>
+//         <div className='zip-code'>
+//           {reportContext.output.A_location.zipcode}
+//         </div>
+//         <div className='city-name'>
+//           <h2>Average Home Price in this Area</h2>
+//           ${reportContext.output.A_location.average_home_price}
+//         </div>
+//       </section>
+//
+//     </>
+//   )
+// };
--- src/components/ReportSecThree/ReportSecThree.tsx
+++ src/components/ReportSecThree/ReportSecThree.tsx
@@ -1,31 +1,31 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ReportContext } from "../../helpers/context";
 
-export const ReportSecThree = () => {
-
-  const reportContext: any = useContext(ReportContext['03_attributes']);
-
-  return (
-    <>
-      <section className='plans-section'>
-        <h1>Your Path Forward</h1>
-        <div className='downpayment'>
-          <div className='dp-saved'>
-            <h2>Your Amount Already Saved Toward Your Downpayment</h2>
-            <h3>{reportContext.output.D_downpayment.information}</h3>
-            ${reportContext.output.D_downpayment.downpayment_saved}
-            or
-            {reportContext.output.D_downpayment.downpayment_percent_saved}% of total principal
-          </div>
-          <div className='dp-percentage'>
-            <h2>Your Downpayment percentage of Principle</h2>
-            {reportContext.output.D_downpayment.downpayment_percentage_selected}%
-          </div>
-        </div>
-        <div className='saving-plans'>
-          // plan otion buttons
-        </div>
-      </section>
-    </>
-  )
-}
+// export const ReportSecThree = () => {
+//
+//   const reportContext: any = useContext(ReportContext['03_attributes']);
+//
+//   return (
+//     <>
+//       <section className='plans-section'>
+//         <h1>Your Path Forward</h1>
+//         <div className='downpayment'>
+//           <div className='dp-saved'>
+//             <h2>Your Amount Already Saved Toward Your Downpayment</h2>
+//             <h3>{reportContext.output.D_downpayment.information}</h3>
+//             ${reportContext.output.D_downpayment.downpayment_saved}
+//             or
+//             {reportContext.output.D_downpayment.downpayment_percent_saved}% of total principal
+//           </div>
+//           <div className='dp-percentage'>
+//             <h2>Your Downpayment percentage of Principle</h2>
+//             {reportContext.output.D_downpayment.downpayment_percentage_selected}%
+//           </div>
+//         </div>
+//         <div className='saving-plans'>
+//           // plan otion buttons
+//         </div>
+//       </section>
+//     </>
+//   )
+// }
